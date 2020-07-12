db7ffa309369a5a4ee5ce16d01069777f1e30665
your Shubinder Virk <shubinder92@gmail.com>
2020-07-12 17:01:29 +0100

    Added the App and Calculator component with tests

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Calculator extends Component {
+  state = {
+    displayValue: "0",
+    numbers: [],
+    operators: [],
+    selectedOperator: "",
+    storedValue: "",
+  };
+
+  callOperator = () => {
+    console.log("call operation");
+  };
+
+  setOperator = () => {
+    console.log("set operation");
+  };
+
+  updateDisplay = () => {
+    console.log("update display");
+  };
+
+  render = () => {
+    return <div className="app-container" />;
+  };
+}
+export default Calculator;
